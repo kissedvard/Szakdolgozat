6c169f9ded47bdb641dfc4df644f922594edbbaf
Kiss Edvárd <kissedvard22@gmail.com>
2026-04-11 01:21:02 +0200

    UI improvements and Login/Register page

---
+++ OkosBufeWeb/ViewModels/ProfileViewModel.cs
@@ -0,0 +1,8 @@
+namespace OkosBufeWeb.ViewModels
+{
+    public class ProfileViewModel
+    {
+        public string FullName {get; set;} = string.Empty;
+        public string Email {get; set;} = string.Empty;
+    }
+}
